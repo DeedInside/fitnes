46f47ef65360a891de38a3b7f8f4ebf1e76fff9a
Пауль Сергей Александрович <paul_s_a@mail.ru>
2023-05-17 21:28:08 +0700

    v1

---
+++ beckend(ASP.net core)/beckend.DALL/Repositories/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using beckend.Domain.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace beckend.DALL.Repositories
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<User> UsersContext { get; set; } = null!;
+
+        public ApplicationContext() 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=fitnesDB;Username=postgres;Password=123");
+        }
+
+    }
+}
